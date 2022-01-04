d5cd7c5c955f24a4ea2b0d95cdb30014e909f1ec
hjkimGithub <rlaguswo3408@gmail.com>
2022-01-04 16:01:26 +0900

    fast url checker

--- main.go
+++ main.go
@@ -2,24 +2,49 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/hjkimGithub/learnGo/mydict"
+	"net/http"
 )
 
-func main() {
-	dictionary := mydict.Dictionary{}
-	word := "hello"
-	dictionary.Add(word, "First")
-	fmt.Println(word)
-	dictionary.Update(word, "Helloo")
-	word, _ = dictionary.Search(word)
-	fmt.Println(word)
-	dictionary.Delete(word)
-	_, err := dictionary.Search(word)
-	fmt.Println(err)
-	// err := dictionary.Update("bye", "BYEBYE")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
+type requestResult struct {
+	url    string
+	status string
+}
 
+func main() {
+	// var results = map[string]string {}
+	results := make(map[string]string)
+	c := make(chan requestResult)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+
+	for _, urls := range urls {
+		go hitURL(urls, c)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
+}
+
+func hitURL(url string, c chan<- requestResult) {
+	resp, err := http.Get(url)
+	status := "OK"
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILED"
+	}
+	c <- requestResult{url: url, status: status}
 }
