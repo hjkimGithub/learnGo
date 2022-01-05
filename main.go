79cf8858d4e9e99bc1704e9a1035c5357136df80
hjkimGithub <rlaguswo3408@gmail.com>
2022-01-05 12:40:30 +0900

    file deletion

--- main.go
+++ main.go
@@ -1,20 +1,24 @@
 package main
 
 import (
+	"os"
 	"strings"
 
 	"github.com/hjkimGithub/learnGo/scrapper"
 	"github.com/labstack/echo"
 )
 
+const fileName string = "jobs.csv"
+
 func handleHome(c echo.Context) error {
 	return c.File("home.html")
 }
 
 func handleScrape(c echo.Context) error {
+	defer os.Remove(fileName)
 	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
 	scrapper.Scrape(term)
-	return c.Attachment("jobs.csv", "job.csv")
+	return c.Attachment(fileName, fileName)
 }
 
 func main() {
