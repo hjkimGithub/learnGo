25b960f5e0d382b0d6f66dbbdb064641aa46d499
hjkimGithub <rlaguswo3408@gmail.com>
2022-01-05 10:48:28 +0900

    apply go routine

--- main.go
+++ main.go
@@ -53,6 +53,7 @@ func writeJobs(jobs []extractedJob) {
 
 func getPage(page int) []extractedJob {
 	var jobs []extractedJob
+	c := make(chan extractedJob)
 	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
 	fmt.Println("Requesting", pageURL)
 	res, err := http.Get(pageURL)
@@ -64,19 +65,24 @@ func getPage(page int) []extractedJob {
 
 	searchCards := doc.Find(".tapItem")
 	searchCards.Each(func(i int, card *goquery.Selection) {
-		job := extractJob(card)
-		jobs = append(jobs, job)
+		go extractJob(card, c)
 	})
+
+	for i := 0; i < searchCards.Length(); i++ {
+		job := <-c
+		jobs = append(jobs, job)
+	}
+
 	return jobs
 }
 
-func extractJob(card *goquery.Selection) extractedJob {
+func extractJob(card *goquery.Selection, c chan<- extractedJob) {
 	id, _ := card.Attr("data-jk")
 	title := cleanString(card.Find(".jobTitle>span").Text())
 	location := cleanString(card.Find(".companyLocation").Text())
 	summary := cleanString(card.Find(".job-snippet").Text())
 	fmt.Println(id, title, location, summary)
-	return extractedJob{id: id, title: title, location: location, summary: summary}
+	c <- extractedJob{id: id, title: title, location: location, summary: summary}
 }
 
 func cleanString(str string) string {
