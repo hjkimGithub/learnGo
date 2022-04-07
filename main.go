d8e68f9ec92c6d1c5f6cff9d3eeab58ca782a26a
hjkim <rlaguswo3408@gmail.com>
2022-04-07 21:12:12 +0900

    fmt POC, standard input

--- main.go
+++ main.go
@@ -3,28 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	a := 123
-	b := 456
-	c := 123456789
+	var a int
+	var b int
 
-	fmt.Printf("%5d, %5d\n", a, b)
-	fmt.Printf("%05d, %05d\n", a, b)
-	fmt.Printf("%-5d, %-5d\n", a, b)
-
-	fmt.Printf("%5d, %5d\n", c, c)
-	fmt.Printf("%05d, %05d\n", c, c)
-	fmt.Printf("%-5d, %-5d\n", c, c)
-
-	d := 324.13455
-	e := 3.14
-
-	fmt.Printf("%08.2f\n", d)
-	fmt.Printf("%08.2g\n", d)
-	fmt.Printf("%8.5g\n", d)
-	fmt.Printf("%f\n", e)
-
-	str := "Hello\tGo\t\tWorld\n\"Go\"is Awesome!\n\\"
-	fmt.Print(str)
-	fmt.Printf("%s", str)
-	fmt.Printf("%q", str)
+	// n, err := fmt.Scan(&a, &b)
+	// n, err := fmt.Scanf(&a, &b)
+	n, err := fmt.Scanln(&a, &b)
+	if err != nil {
+		fmt.Println(n, err)
+	} else {
+		fmt.Println(n, err)
+	}
 }
