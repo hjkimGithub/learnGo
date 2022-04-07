6052881edb800eee930d4f3b9fe2fb132c33b0be
hjkim <rlaguswo3408@gmail.com>
2022-04-07 20:39:52 +0900

    setting recap environment

--- scrapper/scrapper.go
+++ scrapper/scrapper.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	// "github.com/labstack/echo"
 )
 
 type extractedJob struct {
@@ -123,3 +124,30 @@ func checkCode(res *http.Response) {
 		log.Fatalln("Request failed with Status:", res.StatusCode)
 	}
 }
+
+////////////////////////////////// scrapperPOC
+
+/*
+
+const fileName string = "jobs.csv"
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func handleScrape(c echo.Context) error {
+	defer os.Remove(fileName)
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	scrapper.Scrape(term)
+	return c.Attachment(fileName, fileName)
+}
+
+func scrapperPOC() {
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":1323"))
+	// scrapper.Scrape("java")
+}
+
+*/
