32781c0dc96f1b73e9829c27a3fa7597884f2e15
hjkim <rlaguswo3408@gmail.com>
2022-04-07 20:57:41 +0900

    fmt POC, type practice

--- main.go
+++ main.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(1)
+	a := 3
+	var b float64 = 3.5
+
+	var c int = int(b)
+	d := float64(a * c)
+
+	var e int64 = 7
+	f := int64(d) * e
+
+	var g int = int(b * 3)
+	var h int = int(b) * 3
+	fmt.Println(g, h, f)
+
+	var i float32 = 1234.523
+	var j float32 = 3456.123
+	var k float32 = i * j
+	var l float32 = k * 3
+
+	fmt.Println(i, j, k, l)
 }
