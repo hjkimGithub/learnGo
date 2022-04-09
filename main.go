b9f77c4b1b4ebfae9587352a979bc49f01a97676
hjkim <rlaguswo3408@gmail.com>
2022-04-09 15:44:22 +0900

    pointer POC

--- main.go
+++ main.go
@@ -3,11 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	foods := []string{"potato", "pizza", "pasta"}
-	// for i := 0; i < len(foods); i++ {
-	// 	fmt.Println(foods[i])
-	// }
-	fmt.Printf("%v\n", foods)
-	foods = append(foods, "tomato")
-	fmt.Printf("%v\n", foods)
+	a := 2
+	fmt.Println(a, &a)
+	b := &a
+	fmt.Println(a, &a, b, *b)
+	a = 12
+	fmt.Println(a, &a, b, *b)
 }
