ee13d9de9b2deed629f2dbfc5d4c886752d6dd39
hjkim <rlaguswo3408@gmail.com>
2022-04-10 20:00:44 +0900

    memory Padding POC

--- main.go
+++ main.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/hjkimGithub/learnGo/person"
+	"unsafe"
 )
 
+type User struct {
+	Age   int32
+	Score float64
+}
+
+type User2 struct {
+	A int8
+	B int
+	C int8
+	D int
+	E int8
+}
+
+type User3 struct {
+	A int8
+	B int8
+	C int8
+	D int
+	E int
+}
+
 func main() {
-	nico := person.Person{}
-	nico.SetDetails("nico", 12)
-	fmt.Println(nico)
-	nico.SetDetails2("nico", 12)
-	fmt.Println(nico)
+	user2 := User2{1, 2, 3, 4, 5}
+	user3 := User3{1, 2, 3, 4, 5}
+	fmt.Println(unsafe.Sizeof(user2), unsafe.Sizeof(user3))
 }
