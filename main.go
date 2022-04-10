b89d422bce48ad2634f706466ec08df30298d22d
hjkim <rlaguswo3408@gmail.com>
2022-04-10 20:53:22 +0900

    string, rune tick, backtick POC

--- main.go
+++ main.go
@@ -2,17 +2,28 @@ package main
 
 import "fmt"
 
-type User struct {
-	Name string
-	Age  int
-}
-
-func NewUser(name string, age int) *User {
-	var u = User{name, age}
-	return &u
-}
-
 func main() {
-	userPointer := NewUser("AAA", 23)
-	fmt.Println(userPointer)
+	// 	str1 := "Hello\n'World'\t\n"
+	// 	str2 := `Hello
+	// 'World'
+	// `
+	// 	fmt.Println(str1)
+	// 	fmt.Println()
+	// 	fmt.Println(str2)
+
+	str3 := "Hello 월드!"
+	runes := []rune(str3)
+
+	fmt.Println(len(str3), len(runes))
+
+	for i := 0; i < len(runes); i++ {
+		// fmt.Printf("%T, %d, %c\n", str3[i], str3[i], str3[i])
+		fmt.Printf("%T, %d, %c\n", runes[i], runes[i], runes[i])
+	}
+
+	fmt.Println()
+
+	for _, v := range str3 {
+		fmt.Printf("%T, %d, %c\n", v, v, v)
+	}
 }
