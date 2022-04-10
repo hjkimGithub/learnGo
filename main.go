26788082a57255c7423f3c3c40102d36cf244ddc
hjkim <rlaguswo3408@gmail.com>
2022-04-10 20:30:12 +0900

    structure, pointer instance new()

--- main.go
+++ main.go
@@ -2,57 +2,33 @@ package main
 
 import "fmt"
 
-type Cat struct {
-	name string
+type Data struct {
+	value int
+	data  [200]int
 }
 
-// (a Cat) -> Cat 전용 메서드
-func (a Cat) cry() {
-	fmt.Println("야옹~")
-}
-
-type Animal struct {
-	name string
-}
-
-func (a Animal) introduce() {
-	fmt.Println("애완동물의 이름은 " + a.name + "입니다.")
-}
-
-type Hamster struct {
-	// Animal 구조체의 자료형만 선언하면 상속과 같은 역할
-	Animal
-}
-
-func (h Hamster) introduce() {
-	fmt.Println("내 햄스터 이름은 " + h.name + "입니다.")
-}
-
-type Dog struct {
-	// Animal 구조체의 자료형만 선언하면 상속과 같은 역할
-	Animal
-}
-
-func (d Dog) introduce() {
-	fmt.Println("내 강아지의 이름은 " + d.name + "입니다.")
+// func ChangeData(arg Data) {
+func ChangeData(arg *Data) {
+	arg.value = 999
+	arg.data[100] = 999
 }
 
 func main() {
-	kitty := Cat{"나비"}
-	kitty.cry() // 야옹~
-	// cry()       // error
+	var data Data
+	// ChangeData(data)
+	ChangeData(&data)
+	fmt.Println(data.value, data.data[100])
 
-	a := Animal{"통통이"}
-	mouse := Hamster{}
-	puppy := Dog{}
+	// p1 only instance
+	var p1 *Data = &Data{}
+	var p2 *Data = p1
+	var p3 *Data = p1
 
-	mouse.Animal.name = "나비"
-	puppy.Animal.name = "뭉치"
+	// data1, data2, data3 all are instances
+	var data1 Data
+	var data2 Data = data1
+	var data3 Data = data2
 
-	a.introduce()     // 내 애완동물의 이름은 꿀꿀이입니다.
-	mouse.introduce() // 내 애완동물의 이름은 나비입니다.
-	puppy.introduce() // 내 애완동물의 이름은 뭉치입니다.
-
-	mouse.Animal.introduce() // 내 애완동물의 이름은 나비입니다.
-	puppy.Animal.introduce() // 내 애완동물의 이름은 뭉치입니다.
+	var p4 = &Data{}
+	var p5 = new(Data)
 }
