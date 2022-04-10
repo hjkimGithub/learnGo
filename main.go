f2e4ae5f2f2c84bba80ad629ff66bc7726b9bd28
hjkim <rlaguswo3408@gmail.com>
2022-04-10 20:33:41 +0900

    stack & heap memory dangling pointer test

--- main.go
+++ main.go
@@ -2,33 +2,17 @@ package main
 
 import "fmt"
 
-type Data struct {
-	value int
-	data  [200]int
+type User struct {
+	Name string
+	Age  int
 }
 
-// func ChangeData(arg Data) {
-func ChangeData(arg *Data) {
-	arg.value = 999
-	arg.data[100] = 999
+func NewUser(name string, age int) *User {
+	var u = User{name, age}
+	return &u
 }
 
 func main() {
-	var data Data
-	// ChangeData(data)
-	ChangeData(&data)
-	fmt.Println(data.value, data.data[100])
-
-	// p1 only instance
-	var p1 *Data = &Data{}
-	var p2 *Data = p1
-	var p3 *Data = p1
-
-	// data1, data2, data3 all are instances
-	var data1 Data
-	var data2 Data = data1
-	var data3 Data = data2
-
-	var p4 = &Data{}
-	var p5 = new(Data)
+	userPointer := NewUser("AAA", 23)
+	fmt.Println(userPointer)
 }
