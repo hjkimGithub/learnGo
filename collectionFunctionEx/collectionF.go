caca57ee4c3fcba1ae090dc3e78d905361f45b8b
hjkim <rlaguswo3408@gmail.com>
2022-04-14 08:58:40 +0900

    simple functional programming

---
+++ collectionFunctionEx/collectionF.go
@@ -0,0 +1,19 @@
+package collectionF
+
+func Map(vs []int, f func(int) int) []int {
+	vsm := make([]int, len(vs))
+	for i, v := range vs {
+		vsm[i] = f(v)
+	}
+	return vsm
+}
+
+func Filter(vs []string, f func(string) bool) []string {
+	vsf := make([]string, 0)
+	for _, v := range vs {
+		if f(v) {
+			vsf = append(vsf, v)
+		}
+	}
+	return vsf
+}
--- main.go
+++ main.go
@@ -1,18 +1,23 @@
 package main
 
-type Stringer interface {
-	String()
-}
+import (
+	"fmt"
+	"strings"
 
-type Reader interface {
-	Read()
-}
+	collectionF "github.com/hjkimGithub/learnGo/collectionFunctionEx"
+)
 
-func CheckAndRun(stringer Stringer) {
-	if r, ok := stringer.(Reader); ok {
-		r.Read()
-	}
+func add10(n int) int {
+	return n + 10
 }
 
 func main() {
+	strs1 := []int{1, 2, 3}
+	fmt.Println(collectionF.Map(strs1, add10))
+
+	strs2 := []string{"peach", "apple", "pear", "plum"}
+	fmt.Println(collectionF.Filter(strs2, func(s string) bool {
+		return strings.Contains(s, "e")
+	}))
+
 }
