f5793c6272414b066746d681efb5239ef335e27b
hjkim <rlaguswo3408@gmail.com>
2022-04-13 19:37:13 +0900

    duck typing POC

--- main.go
+++ main.go
@@ -1,60 +1,26 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
 
-	"github.com/hjkimGithub/learnGo/person"
-	"github.com/hjkimGithub/learnGo/shape"
-)
+type Stringer interface {
+	String() string
+}
+
+// type Student struct implements Stringer
+type Student struct { // duck typing
+	Name string
+	Age  int
+}
+
+func (s Student) String() string {
+	return fmt.Sprintf("안녕! 나는 %d 살, %s 라고 해", s.Age, s.Name)
+}
 
 func main() {
-	// github.com/hjkimGithub/learnGo/shape.Point composite literal uses unkeyed fieldscomposites
-	p := shape.Point{3, 4}
-	p.Add(10)  // 적용됨 13, 14
-	p.Mul(100) // 적용 불가 13, 14 그대로
-	fmt.Println(p)
+	student := Student{"철수", 12}
+	var stringer Stringer
 
-	// structure init with no error
-	s := shape.Square{
-		Width:  2,
-		Height: 5,
-	}
-	t := shape.Triangle{
-		Width:  2,
-		Height: 5,
-	}
-	fmt.Println(s.Area())
-	fmt.Println(t.Area())
+	stringer = student
 
-	var s1, t1 shape.Figure
-	s1 = shape.Square{
-		Width:  2,
-		Height: 5,
-	}
-	t1 = shape.Triangle{
-		Width:  2,
-		Height: 5,
-	}
-	fmt.Println(s1.Area())
-	fmt.Println(t1.Area())
-
-	f := make([]shape.Figure, 0)
-	f = append(f, shape.Square{
-		Width:  2,
-		Height: 5,
-	})
-	f = append(f, shape.Triangle{
-		Width:  2,
-		Height: 5,
-	})
-	// struct가 interface형으로 구현될려면 interface에 정의된 메소드가 struct에 존재해야한다.
-	// f = append(f, Line{Length: 5})
-	for _, value := range f {
-		fmt.Println(value.Area())
-	}
-
-	i := person.Person{Name: "Kayle", Age: 20}
-	// i.SetDetails2("KAYLE", 20)
-	i.HisName()
-	i.HisAge()
+	fmt.Println(stringer.String())
 }
