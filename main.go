7469107680a727f9e053d689fd22aae277bb45ba
hjkim <rlaguswo3408@gmail.com>
2022-04-13 19:56:40 +0900

    interface error handle

--- main.go
+++ main.go
@@ -1,47 +1,18 @@
 package main
 
-import "fmt"
-
 type Stringer interface {
-	String() string
+	String()
 }
 
-type Student struct {
-	Age int
+type Reader interface {
+	Read()
 }
 
-type Student2 struct {
-}
-
-type Actor struct {
-}
-
-func (a *Actor) String() string {
-	return "Actor"
-}
-
-func (s *Student) String() string {
-	return fmt.Sprintf("Student Age: %d", s.Age)
-}
-
-func PrintAge(stringer Stringer) {
-	s := stringer.(*Student)
-	fmt.Printf("Age: %d\n", s.Age)
-}
-
-func ConvertType(stringer Stringer) {
-	student := stringer.(*Student)
-	fmt.Println(student)
+func CheckAndRun(stringer Stringer) {
+	if r, ok := stringer.(Reader); ok {
+		r.Read()
+	}
 }
 
 func main() {
-	s := &Student{15}
-	PrintAge(s)
-
-	// var stringer Stringer
-	// compile time error, impossible type asssertion
-	// stringer.(*Student2)
-
-	actor := &Actor{}
-	ConvertType(actor)
 }
