5af596343eb11e6603a1e06abfea30e086c2878b
hjkim <rlaguswo3408@gmail.com>
2022-04-14 19:24:44 +0900

    simple file rw

---
+++ filerw/filerw.go
@@ -0,0 +1,28 @@
+package filerw
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func ReadFile(filename string) (string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+	rd := bufio.NewReader(file)
+	line, _ := rd.ReadString('\n')
+	return line, nil
+}
+
+func WriteFile(filename string, line string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = fmt.Fprintln(file, line)
+	return err
+}
--- main.go
+++ main.go
@@ -1,35 +1,26 @@
 package main
 
 import (
-	"container/list"
 	"fmt"
 
-	"github.com/hjkimGithub/learnGo/datastructure"
+	"github.com/hjkimGithub/learnGo/filerw"
 )
 
+const filename string = "data.txt"
+
 func main() {
-	v := list.New()
-	e4 := v.PushBack(4)
-	e1 := v.PushFront(1)
-	v.InsertBefore(3, e4)
-	v.InsertAfter(2, e1)
-
-	for e := v.Front(); e != nil; e = e.Next() {
-		fmt.Print(e.Value, " ")
+	line, err := filerw.ReadFile(filename)
+	if err != nil {
+		err = filerw.WriteFile(filename, "switch a text")
+		if err != nil {
+			fmt.Println("파일 생성 실패", err)
+			return
+		}
+		line, err = filerw.ReadFile(filename)
+		if err != nil {
+			fmt.Println("파일 읽기 실패", err)
+			return
+		}
 	}
-	fmt.Println()
-	for e := v.Back(); e != nil; e = e.Prev() {
-		fmt.Print(e.Value, " ")
-	}
-
-	fmt.Println()
-	queue := datastructure.NewQueue()
-	for i := 1; i < 5; i++ {
-		queue.Push(i)
-	}
-	v2 := queue.Pop()
-	for v2 != nil {
-		fmt.Printf("%d ->\n", v)
-		v2 = queue.Pop()
-	}
+	fmt.Println(line)
 }
